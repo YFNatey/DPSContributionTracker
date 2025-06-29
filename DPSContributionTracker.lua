1a6cb9f7b11f90876f1ca5763012a6144d761b8f
YFNatey <yfnatey@gmail.com>
2025-06-28 17:30:04 -0700

    Create functions to get boss health, track player damage, and track updates boss health

--- DPSContributionTracker.lua
+++ DPSContributionTracker.lua
@@ -6,6 +6,40 @@ DPSContributionTracker.savedVars = nil
 DPSContributionTracker.totalBossMaxHealth = 0
 DPSContributionTracker.totalBossCurrentHealth = 0
 DPSContributionTracker.playerDamage = 0
+DPSContributionTracker.currentBossHealth = 0
+DPSContributionTracker.maxBossHealth = 0
+
+--Get total boss health
+function DPSContributionTracker:GetBossHealth()
+    self.currentBossHealth = 0
+    self.maxBossHealth = 0
+    for i = 1, 5 do
+        local unitTag = "boss" .. i
+        if DoesUnitExist(unitTag) then
+            self.currentBossHealth = self.currentBossHealth + GetUnitHealth(unitTag)
+            self.maxBossHealth = self.maxBossHealth + GetUnitMaxHealth(unitTag)
+        end
+    end
+end
+
+-- Called every combat event to track player damage
+function DPSContributionTracker:OnCombatEvent(_, _, _, _, _, _, sourceName, _, _, _, hitValue, _, _, _, _, targetUnitId,
+                                              _, _)
+    if sourceName == GetUnitName("player") and hitValue > 0 and (targetUnitId and IsUnitBoss(targetUnitId)) then
+        self.playerDamage = self.playerDamage + hitValue
+    end
+end
+
+-- Called to update the boss health and print DPS info
+function DPSContributionTracker:UpdateStatus()
+    self.GetBossHealth()
+    if self.maxBossHealth > 0 then
+        local damageDone = self.maxBossHealth - self.currentBossHealth
+        local damagePercent = (damageDone / self.maxBossHealth) * 100
+        d(string.format("Boss HP: %d / %d (%.2f%%)", self.currentBossHealth, self.maxBossHealth, damagePercent))
+        d(string.format("Your damage: %d", self.playerDamage))
+    end
+end
 
 local function Initialize()
     -- Initialize saved variables
